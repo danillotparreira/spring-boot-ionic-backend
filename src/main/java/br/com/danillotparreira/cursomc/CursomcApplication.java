ad9d621fdef3f0f4cb192420cbab54d8a3d7d33c
unknown <parreira16@gmail.com>
2020-10-27 19:43:26 -0300

    Criado operação de instanciação

--- src/main/java/br/com/danillotparreira/cursomc/CursomcApplication.java
+++ src/main/java/br/com/danillotparreira/cursomc/CursomcApplication.java
@@ -1,13 +1,28 @@
 package br.com.danillotparreira.cursomc;
 
+import br.com.danillotparreira.cursomc.model.Categoria;
+import br.com.danillotparreira.cursomc.repositories.CategoriaRepository;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursomcApplication{
+public class CursomcApplication implements CommandLineRunner {
 
   public static void main(String[] args) {
     SpringApplication.run(CursomcApplication.class, args);
   }
 
+  // Configuração da operação de instanciação, com a implementação do CommandLineRunner
+  @Autowired
+  private CategoriaRepository categoriaRepository;
+
+  @Override
+  public void run(String... args) throws Exception {
+    Categoria cat1 = new Categoria("Informática");
+    Categoria cat2 = new Categoria("Escritório");
+    categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+  }
 }
