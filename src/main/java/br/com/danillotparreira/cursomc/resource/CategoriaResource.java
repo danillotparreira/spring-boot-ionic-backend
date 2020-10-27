8518c3d4ad90b16949aedf3bd03af43f735c5267
unknown <parreira16@gmail.com>
2020-10-27 16:45:08 -0300

    Testando a primeira classe de modelo

--- src/main/java/br/com/danillotparreira/cursomc/resource/CategoriaResource.java
+++ src/main/java/br/com/danillotparreira/cursomc/resource/CategoriaResource.java
@@ -1,5 +1,8 @@
 package br.com.danillotparreira.cursomc.resource;
 
+import br.com.danillotparreira.cursomc.model.Categoria;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 
   @GetMapping
-  public String listar() {
-    return "Rest testado.";
+  public List<Categoria> listar() {
+    Categoria cat1 = new Categoria(1, "Informática");
+    Categoria cat2 = new Categoria(2, "Escritório");
+    List<Categoria> categorias = new ArrayList<>();
+    categorias.add(cat1);
+    categorias.add(cat2);
+    return categorias;
   }
 }
